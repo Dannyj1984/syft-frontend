41fa21eae160009dcc70899a0260e6e909ef081a
Danny J <dj3636@ou.ac.uk>
2021-11-12 22:06:03 +0000

    update

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 //user calls
 
 export const signup = (user) => {
-  return axios.post('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/users', user);
+  return axios.post('/api/1.0/management/users', user);
 };
 
 export const login = (user) => {
-  return axios.post('https://glacial-sierra-91195.herokuapp.com/api/1.0/login', {}, { auth: user });
+  return axios.post('/api/1.0/login', {}, { auth: user });
 };
 
 export const setAuthorizationHeader = ({ username, password, isLoggedIn }) => {
@@ -21,114 +21,114 @@ export const setAuthorizationHeader = ({ username, password, isLoggedIn }) => {
 };
 
 export const listUsers = (param = { page: 0, size: 9 }) => {
-  const path = `https://glacial-sierra-91195.herokuapp.com/api/1.0/users?page=${param.page || 0}&size=${param.size || 9}&sort=username,asc`;
+  const path = `/api/1.0/users?page=${param.page || 0}&size=${param.size || 9}&sort=username,asc`;
   return axios.get(path);
 };
 
 export const getUser = (username) => {
-  return axios.get(`https://glacial-sierra-91195.herokuapp.com/api/1.0/users/${username}`);
+  return axios.get(`/api/1.0/users/${username}`);
 };
 
 export const updateUser = (userId, body) => {
-  return axios.put('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/users/' + userId, body);
+  return axios.put('/api/1.0/management/users/' + userId, body);
 };
 
 export const deleteMember = (userid) => {
-  return axios.delete('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/users/delete/' + userid);
+  return axios.delete('/api/1.0/management/users/delete/' + userid);
 };
 
 export const makeAdmin = (userid) => {
-  return axios.put('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/users/admin/' + userid);
+  return axios.put('/api/1.0/management/users/admin/' + userid);
 };
 
 export const makeHcpAdmin = (userid) => {
-  return axios.put('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/users/HcpAdmin/' + userid);
+  return axios.put('/api/1.0/management/users/HcpAdmin/' + userid);
 };
 
 export const makeEventAdmin = (userid) => {
-  return axios.put('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/users/EventAdmin/' + userid);
+  return axios.put('/api/1.0/management/users/EventAdmin/' + userid);
 };
 
 export const makeUser = (userid) => {
-  return axios.put('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/users/User/' + userid);
+  return axios.put('/api/1.0/management/users/User/' + userid);
 };
 
 export const updateHandicap = (userid, body) => {
-  return axios.put('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/users/handicap/' + userid, body);
+  return axios.put('api/1.0/management/users/handicap/' + userid, body);
 }
 
 //course calls
 
 export const signupCourse = (course) => {
-  return axios.post('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/courses', course);
+  return axios.post('/api/1.0/management/courses', course);
 };
 
 export const getCourses = () => {
-  return axios.get(`https://glacial-sierra-91195.herokuapp.com/api/1.0/management/courses`);
+  return axios.get(`/api/1.0/management/courses`);
 };
 
 export const listCourses = (param = { page: 0, size: 9 }) => {
-  const path = `https://glacial-sierra-91195.herokuapp.com/api/1.0/courses?page=${param.page || 0}&size=${param.size || 9}&sort=courseName,asc`;
+  const path = `/api/1.0/courses?page=${param.page || 0}&size=${param.size || 9}&sort=courseName,asc`;
   return axios.get(path);
 };
   
 export const getCourse = (coursename) => {
-  return axios.get(`https://glacial-sierra-91195.herokuapp.com/api/1.0/courses/${coursename}`);
+  return axios.get(`/api/1.0/courses/${coursename}`);
 };
 
 export const updateCourse = (courseId, body) => {
-  return axios.put('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/courses/' + courseId, body);
+  return axios.put('/api/1.0/management/courses/' + courseId, body);
 };
 
 export const deleteCourse = (courseId) => {
-  return axios.delete('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/courses/delete/' + courseId);
+  return axios.delete('/api/1.0/management/courses/delete/' + courseId);
 };
 
 export const addHoleDetails = (list) => {
-  return axios.post('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/holes', list);
+  return axios.post('/api/1.0/management/holes', list);
 };
 
 export const getCourseHoles = (courseid)=> {
-  return axios.get('https://glacial-sierra-91195.herokuapp.com/api/1.0/getListOfHoles/' + courseid);
+  return axios.get('/api/1.0/getListOfHoles/' + courseid);
 }
 
 //Event calls
 
 export const signupEvent = (event) => {
-  return axios.post('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/events', event);
+  return axios.post('/api/1.0/management/events', event);
 };
 
 export const listEvents = (param = { page: 0, size: 9 }) => {
-  const path = `https://glacial-sierra-91195.herokuapp.com/api/1.0/events?page=${param.page || 0}&size=${param.size || 9}&sort=eventname,asc`;
+  const path = `/api/1.0/events?page=${param.page || 0}&size=${param.size || 9}&sort=eventname,asc`;
   return axios.get(path);
 };
   
 export const getEvent = (eventname) => {
-  return axios.get(`https://glacial-sierra-91195.herokuapp.com/api/1.0/events/${eventname}`);
+  return axios.get(`/api/1.0/events/${eventname}`);
 };
 
 export const getEvents = () => {
-  return axios.get(`https://glacial-sierra-91195.herokuapp.com/api/1.0/management/events`);
+  return axios.get(`/api/1.0/management/events`);
 };
 
 export const updateEvent = (eventId, body) => {
-  return axios.put('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/events/' + eventId, body);
+  return axios.put('/api/1.0/management/events/' + eventId, body);
 };
 
 export const deleteEvent = (eventId) => {
-  return axios.delete('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/events/delete/' + eventId);
+  return axios.delete('/api/1.0/management/events/delete/' + eventId);
 };
 
 export const getCourseDetails = (eventid) => {
-  return axios.get('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/events/courseDetails/' + eventid);
+  return axios.get('/api/1.0/management/events/courseDetails/' + eventid);
 }
 
 export const eventEnter = (entrant) => {
-  return axios.post('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/events/entrants', entrant);
+  return axios.post('/api/1.0/management/events/entrants', entrant);
 }
 
 //Society calls
 
 export const signupSociety = (society) => {
-  return axios.post('https://glacial-sierra-91195.herokuapp.com/api/1.0/management/society', society);
+  return axios.post('/api/1.0/management/society', society);
 };
